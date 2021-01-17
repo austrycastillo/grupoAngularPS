79591a6331747cea28480622ab6315acf2651849
Austry Castillo <castilloaustry@gmail.com>
2021-01-17 12:24:18 -0300

    subiendo

---
+++ proyectito2/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DatosComponent } from './datos/datos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DatosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
